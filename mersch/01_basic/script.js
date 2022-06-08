0e87e6ddcdd4fd427a60f55bd634639299577438
OinkoIT <claus.mersch@gmail.com>
2022-06-08 10:47:40 +0200

    basic

--- mersch/01_basic/script.js
+++ mersch/01_basic/script.js
@@ -3,5 +3,14 @@ let text = "Hello world!";
 console.log(text);
 let text2 = "Hello world!";
 console.log(text);
-
-function multiply
+function multiply(num1) {
+    for (let i = 1; i <= 10; i++) {
+        return i * num1;
+    }
+    ;
+}
+;
+console.log(multiply(1));
+// for (let i=1; i <= 10; i++) {
+//   console.log(i * 1);
+// };
--- mersch/01_basic/script.ts
+++ mersch/01_basic/script.ts
@@ -4,3 +4,17 @@ console.log(text);
 
 let text2 : string = "Hello world!";
 console.log(text);
+
+function multiply (num1: number) {
+  for (let i=1; i <= 10; i++) {
+    return i * num1;
+  };
+};
+
+console.log(multiply(1));
+
+
+  // for (let i=1; i <= 10; i++) {
+  //   console.log(i * 1);
+  // };
+
