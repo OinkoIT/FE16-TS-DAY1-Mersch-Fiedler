206b9e7d654bf857d93b0ba371d84fd53c5e43ea
OinkoIT <claus.mersch@gmail.com>
2022-06-08 10:28:07 +0200

    names

---
+++ mersch/01_basic/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let text = "Hello world!";
+console.log(text);
+let text2 = "Hello world!";
+console.log(text);
+
+function multiply
---
+++ mersch/01_basic/script.ts
@@ -0,0 +1,6 @@
+let text : string = "Hello world!";
+console.log(text);
+
+
+let text2 : string = "Hello world!";
+console.log(text);
